72ed68156f028b40db45d66a185070c099f5c47c
jacobmurillo <jacobmurillo.jm@gmail.com>
2016-11-14 20:30:06 -0600

    Comments added
    comments for days

--- src/warshall.java
+++ src/warshall.java
@@ -2,20 +2,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/**
- * Created by Giampiero Vanzzini on 11/13/2016.
+/*
+ * warshall.java
+ * Exploration 5
+ * November 14, 2016
+ *
+ * Performs Warshall's algorithm from an input file from a
+ * console command and prints out the transitive closure
  *
  */
+
 public class warshall {
 
     public static void main(String[] args) throws FileNotFoundException {
+
+        // get the number row (and col) count from the input file
         Scanner in = new Scanner(new File(args[0]));
         String[] n = in.nextLine().split(" ");
         int count = n.length;
         in.close();
+
+        // start building the matrix from the input file
         in = new Scanner(new File(args[0]));
         int[][] AdjacencyMatrix = new int[count][count];
-        //in.nextLine();
         for(int i = 0; i < count; i ++){
             for(int j = 0; j < count; j++){
                 AdjacencyMatrix[i][j] = in.nextInt();
@@ -23,6 +32,8 @@ public class warshall {
             if(in.hasNextLine())
             in.nextLine();
         }
+
+        // print out the input adjacency matrix
         System.out.println("Adjacency Matrix: ");
         for(int i = 0; i < count; i ++){
             for(int j = 0; j < count; j++){
@@ -31,9 +42,15 @@ public class warshall {
             System.out.println();
         }
         System.out.println();
+
+        // start the timer as soon as Warshall's Algorithm is being run
+        // and stop it when it's done calculating
         long startTime = System.nanoTime();
         int[][] TransitiveClosure = Warshalls(AdjacencyMatrix, count);
         long totalTime = System.nanoTime() - startTime;
+
+
+        // printout the output transitive closure
         System.out.println("Transitive Closure: " );
         for(int i = 0; i < count; i ++){
             for(int j = 0; j < count; j++){
@@ -42,20 +59,21 @@ public class warshall {
             System.out.println();
         }
         System.out.println();
-        System.out.println("Warshall's run time: " + totalTime + " nanoseconds.");
 
+        // printout the total runtime saved from ealier
+        System.out.println("Warshall's run time: " + totalTime + " nanoseconds.");
     }
 
+    // Warshalls - performs Warshall's algorithm on the matrix
+    // Input - adjacency matrix as a double array, and the count of places
+    // Output - returns the modified matrix as a transitive closure
     private static int[][] Warshalls(int[][] adjacencyMatrix, int count) {
-        for(int k = 0; k < count; k++){
-            for(int i = 0; i < count; i++){
-                for(int j = 0; j < count; j++){
-                    if(adjacencyMatrix[i][k] == 1 && adjacencyMatrix[k][j] == 1){
+        for(int k = 0; k < count; k++)
+            for(int i = 0; i < count; i++)
+                for(int j = 0; j < count; j++)
+                    if(adjacencyMatrix[i][k] == 1 && adjacencyMatrix[k][j] == 1)
                         adjacencyMatrix[i][j] = 1;
-                    }
-                }
-            }
-        }
+
         return adjacencyMatrix;
     }
 }
